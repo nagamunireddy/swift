f08f070a25b6436972c1e795f363d08d73b33095
naga <nag.bmsit@gmail.com>
2018-02-23 13:39:21 +0530

    update for foodtracer app

---
+++ Textmagazine/Textmagazine/CTView.swift
@@ -0,0 +1,21 @@
+//
+//  CTView.swift
+//  Textmagazine
+//
+//  Created by Adroitent INC on 23/02/18.
+//  Copyright © 2018 Adroitent INC. All rights reserved.
+//
+
+import UIKit
+
+class CTView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
